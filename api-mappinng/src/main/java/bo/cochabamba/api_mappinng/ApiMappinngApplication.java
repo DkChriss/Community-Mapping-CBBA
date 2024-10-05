f1234c00162316e47904d6879f4b1049a6fed916
Chriss Huayhua <chuanca@cochabamba.bo>
2024-10-05 12:07:56 -0400

    Create project backennd

---
+++ api-mappinng/src/main/java/bo/cochabamba/api_mappinng/ApiMappinngApplication.java
@@ -0,0 +1,13 @@
+package bo.cochabamba.api_mappinng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMappinngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMappinngApplication.class, args);
+	}
+
+}
---
+++ api-mappinng/src/test/java/bo/cochabamba/api_mappinng/ApiMappinngApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.cochabamba.api_mappinng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMappinngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
