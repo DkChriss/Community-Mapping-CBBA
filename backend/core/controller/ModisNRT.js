e41f03954aa2ba99d29994c30a2721c0e58c1289
Chriss Huayhua <chuanca@cochabamba.bo>
2024-10-06 07:52:23 -0400

    Enable Cors

--- backend/core/controller/ModisNRT.js
+++ backend/core/controller/ModisNRT.js
@@ -7,7 +7,10 @@ class ModisNRTController {
     static async list(req, res) {
         try {
             
+            console.log(req.body);
+            
             const recordList = await service.list(req.body);
+            
             const responseList = [];
             
             recordList.map(record => {
