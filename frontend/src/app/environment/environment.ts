776bcaaf40b1408e3710609ca4de28a23464cc21
Chriss Huayhua <chuanca@cochabamba.bo>
2024-10-06 10:28:13 -0400

    Fix svg an styles

--- frontend/src/app/environment/environment.ts
+++ frontend/src/app/environment/environment.ts
@@ -1,8 +1,7 @@
 export const environment = {
     production: false,
 
-    base_url: 'http://192.168.116.88:3000/api/v1/',
-    base_url_public: 'http://192.168.117.89:3000/api/v1/modis_nrt',
-
+    base_url: 'http://localhost:3000/api/v1/',
+    base_url_public: 'http://localhost:3000/api/v1/modis_nrt',
 
   };
--- frontend/src/app/pages/components/fire-map/fire-map.component.ts
+++ frontend/src/app/pages/components/fire-map/fire-map.component.ts
@@ -13,13 +13,13 @@ import { Daum, FireMapDate, Root } from '../../../interfaces/fire-map.interface'
 })
 export class FireMapComponent implements OnInit, OnDestroy {
 
-  
-  loading: boolean = false; 
+
+  loading: boolean = false;
   lg!: FormGroup;
   private map: L.Map | undefined;
 
-  constructor(private fireMapService: FireMapService, 
-              private fb: FormBuilder, 
+  constructor(private fireMapService: FireMapService,
+              private fb: FormBuilder,
               private router: Router,
               private http: HttpClient) {
     this.lg = this.fb.group({
@@ -29,14 +29,16 @@ export class FireMapComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.map = L.map('map').setView([-16.290154, -63.588653], 6);
+    this.map = L.map('map-street').setView([-16.290154, -63.588653], 6);
     L.tileLayer('https://tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey=d38a47705d6e4a5b98761d37eb661fe5', {
       maxZoom: 19,
       attribution: '&copy; <a href="https://www.thunderforest.com/">Thunderforest</a> &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(this.map);
 
-    this.loadGeoJson();
-
+    const loadGeoJson = false;
+    if (loadGeoJson) {
+      this.loadGeoJson();
+    }
     this.loading = true;
     setTimeout(() => {
       this.loading = false;
@@ -44,7 +46,10 @@ export class FireMapComponent implements OnInit, OnDestroy {
   }
 
   private loadGeoJson() {
-    this.http.get('../../../../assets/geojson/gadm41_BOL_3.json').subscribe((geojsonData: any) => {
+
+    const geoJsonUrl = `../../../../assets/geojson/gadm41_BOL_3.json?cacheBust=${new Date().getTime()}`;
+
+    this.http.get(geoJsonUrl).subscribe((geojsonData: any) => {
       const geoJsonStyle = {
         color: '#686868',
         weight: 1.5,
@@ -93,16 +98,77 @@ export class FireMapComponent implements OnInit, OnDestroy {
   }
 
   private handleApiResponse(response: Root) {
-    const markerData = response.data; // Asegúrate de que la respuesta tiene el formato esperado
+    const markerData = response.data;
     this.addMarkers(markerData);
+  }
+
+  private normalizeBrightness(brightness: number): number {
+    const minBrightness = 200;
+    const maxBrightness = 600;
+    return ((brightness - minBrightness) / (maxBrightness - minBrightness)) * 1000;
+  }
+
+  private calculateAffectedKm(brightness: number): number {
+    const baseArea = 0.1;
+    return baseArea * Math.pow(brightness, 0.5);
   }
 
   private addMarkers(markerData: Daum[]) {
     if (this.map) {
-      markerData.forEach(data => {
-        const marker = L.marker([parseFloat(data.latitude), parseFloat(data.longitude)]).addTo(this.map!);
-        marker.bindPopup(`<b>Fecha:</b> ${data.date}<br><b>Brillo:</b> ${data.brightness}<br>`).openPopup();
-      });
+      let index = 0;
+
+      const addMarkerWithDelay = () => {
+        if (index >= markerData.length) {
+          return;
+        }
+
+        const data = markerData[index];
+
+        const customIcon = L.divIcon({
+          className: 'svg-icon-map',
+          html: `
+            <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+              width="2rem" height="2rem" viewBox="0 0 533.333 533.333"
+                style="width: 2rem !important; height: 2rem !important; transform: none !important; enable-background:new 0 0 533.333 533.333;" xml:space="preserve">
+              <defs>
+                <linearGradient id="grad1" x1="0%" y1="100%" x2="0%" y2="0%">
+                  <stop offset="0%" style="stop-color:rgba(131,20,3,1); stop-opacity:1" />
+                  <stop offset="100%" style="stop-color:rgba(247,210,106,1); stop-opacity:1" />
+                </linearGradient>
+              </defs>
+              <g>
+                <path fill="url(#grad1)" d="M165.494,533.333c-35.545-73.962-16.616-116.343,10.703-156.272
+                  c29.917-43.728,37.627-87.013,37.627-87.013s23.518,30.573,14.11,78.39c41.548-46.25,49.389-119.938,43.115-148.159
+                  c93.914,65.63,134.051,207.737,79.96,313.054c287.695-162.776,71.562-406.339,33.934-433.775
+                  c12.543,27.435,14.922,73.88-10.416,96.42C331.635,33.333,225.583,0,225.583,0c12.543,83.877-45.466,175.596-101.404,244.13
+                  c-1.965-33.446-4.053-56.525-21.641-88.531C98.59,216.357,52.157,265.884,39.583,326.76C22.551,409.2,52.341,469.562,165.494,533.333z"/>
+              </g>
+            </svg>`,
+          iconSize: [5, 5],
+          iconAnchor: [2, 2]
+        });
+
+        const normalizedBrightness = this.normalizeBrightness(data.brightness);
+        const affectedKm = this.calculateAffectedKm(normalizedBrightness);
+
+        const marker = L.marker([parseFloat(data.latitude), parseFloat(data.longitude)], { icon: customIcon }).addTo(this.map!);
+
+        const circle = L.circle([parseFloat(data.latitude), parseFloat(data.longitude)], {
+          color: 'red',
+          weight: 1,
+          fillColor: '#f03',
+          fillOpacity: 0.5,
+          radius: affectedKm * 1000
+        }).addTo(this.map!);
+
+        marker.bindPopup(`<b>Fecha:</b> ${data.date}<br><b>Brillo:</b> ${data.brightness}<br><b>Kilómetros afectados:</b> ${affectedKm.toFixed(2)} km`).openPopup();
+
+        index++;
+
+        setTimeout(addMarkerWithDelay, 500);
+      };
+
+      addMarkerWithDelay();
     } else {
       console.error('El mapa no está inicializado');
     }
