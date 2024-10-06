f46872910a28d805f9d8eeb081e21b511b45ac23
Chriss Huayhua <chuanca@cochabamba.bo>
2024-10-06 02:01:25 -0400

    Function list

--- backend/core/dto/ModisNRT.js
+++ backend/core/dto/ModisNRT.js
@@ -4,14 +4,16 @@ class ModisNRTDto {
     longitude;
     date;
     time;
-    brightness
+    brightness;
+    brightness_c;
     
-    constructor(latitude, longitude, date, time, brightness) {
+    constructor(latitude, longitude, date, time, brightness, brightness_c) {
         this.latitude = latitude;
         this.longitude = longitude;
         this.date = date;
         this.time = time;
         this.brightness = brightness;
+        this.brightness_c = brightness_c;
     }
     
 }
